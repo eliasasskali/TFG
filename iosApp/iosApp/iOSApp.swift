56a6c8d3c198a2e3a16df8e1ab1cf8834912baf5
Elias <elias.asskali01@estudiant.upf.edu>
2022-01-17 11:53:53 +0100

    IOS firebase dependencies
    - Crashlytics and auth

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,7 +1,13 @@
 import SwiftUI
+import Firebase
 
 @main
 struct iOSApp: App {
+    init() {
+        FirebaseApp.configure()
+        //fatalError()
+    }
+    
 	var body: some Scene {
 		WindowGroup {
 			ContentView()
