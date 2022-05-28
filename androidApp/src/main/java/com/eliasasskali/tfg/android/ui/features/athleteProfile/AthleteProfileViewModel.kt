aaf9b32bff787c87883610f7e59b43dcd1d2e8d0
Elias <elias.asskali01@estudiant.upf.edu>
2022-05-28 21:43:19 +0200

    Athlete Profile ViewModel and State

---
+++ androidApp/src/main/java/com/eliasasskali/tfg/android/ui/features/athleteProfile/AthleteProfileViewModel.kt
@@ -0,0 +1,94 @@
+package com.eliasasskali.tfg.android.ui.features.athleteProfile
+
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.viewModelScope
+import com.eliasasskali.tfg.android.core.ui.RootViewModel
+import com.eliasasskali.tfg.android.data.repository.AuthRepository
+import com.eliasasskali.tfg.android.data.repository.ClubAthleteRepository
+import com.eliasasskali.tfg.data.preferences.Preferences
+import com.eliasasskali.tfg.model.Athlete
+import com.eliasasskali.tfg.ui.error.ErrorHandler
+import com.eliasasskali.tfg.ui.executor.Executor
+import com.google.gson.Gson
+import kotlinx.coroutines.launch
+
+
+class AthleteProfileViewModel(
+    private val preferences: Preferences,
+    private val authRepository: AuthRepository,
+    private val repository: ClubAthleteRepository,
+    executor: Executor,
+    errorHandler: ErrorHandler
+) : RootViewModel(executor, errorHandler) {
+
+    val state: MutableState<AthleteProfileState> = mutableStateOf(AthleteProfileState())
+
+    fun initAthleteProfileScreen() {
+        setStep(AthleteProfileSteps.IsLoading)
+        val athlete = Gson().fromJson(preferences.getProfileJson(), Athlete::class.java)
+        state.value = state.value.copy(athlete = athlete)
+        getFollowingClubNames(athlete.following)
+        setStep(AthleteProfileSteps.ShowAthleteProfile)
+    }
+
+    fun setStep(step: AthleteProfileSteps) {
+        state.value = state.value.copy(step = step)
+    }
+
+    fun logOut(onLoggedOut: () -> Unit) {
+        viewModelScope.launch {
+            execute {
+                authRepository.logOut()
+            }.fold(
+                error = {
+                    // TODO: Handle error
+                },
+                success = {
+                    onLoggedOut()
+                }
+            )
+        }
+    }
+
+    private fun getFollowingClubNames(following: List<String>) {
+        viewModelScope.launch {
+            execute {
+                repository.getFollowingClubNames(following)
+            }.fold(
+                error = {
+                    // TODO: Handle Error
+                },
+                success = { followingClubs ->
+                    state.value = state.value.copy(followingClubs = followingClubs)
+                }
+            )
+        }
+    }
+
+    fun unFollowClub(clubId: String) {
+        viewModelScope.launch {
+            execute {
+                repository.unFollowClub(clubId)
+            }.fold(
+                error = {},
+                success = {
+                    saveAthletePreferences()
+                }
+            )
+        }
+    }
+
+    private fun saveAthletePreferences() {
+        viewModelScope.launch {
+            execute {
+                repository.saveAthletePreferences()
+            }.fold(
+                error = {},
+                success = {
+                    initAthleteProfileScreen()
+                }
+            )
+        }
+    }
+}
