7aea63d4cde43b8cd534b26212177128ba66c6a1
Elias <elias.asskali01@estudiant.upf.edu>
2022-05-28 21:42:29 +0200

    Auth repository function name change

--- androidApp/src/main/java/com/eliasasskali/tfg/android/data/repository/AuthRepositoryImp.kt
+++ androidApp/src/main/java/com/eliasasskali/tfg/android/data/repository/AuthRepositoryImp.kt
@@ -21,7 +21,7 @@ class AuthRepositoryImp constructor(
         }
     }
 
-    override suspend fun signOut(): Either<DomainError, Success> {
+    override suspend fun logOut(): Either<DomainError, Success> {
         return try {
             Firebase.auth.signOut()
             Either.Right(Success)
