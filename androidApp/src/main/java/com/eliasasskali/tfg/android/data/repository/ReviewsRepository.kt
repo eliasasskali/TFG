28654efcc583bb3c07a95c904be5c959e5f1ff38
Elias <elias.asskali01@estudiant.upf.edu>
2022-05-29 21:54:39 +0200

    Reviews repository

---
+++ androidApp/src/main/java/com/eliasasskali/tfg/android/data/repository/ReviewsRepository.kt
@@ -0,0 +1,10 @@
+package com.eliasasskali.tfg.android.data.repository
+
+import androidx.paging.PagingData
+import com.eliasasskali.tfg.model.*
+import kotlinx.coroutines.flow.Flow
+
+interface ReviewsRepository {
+    fun getReviews(clubId: String): Flow<PagingData<Review>>
+    suspend fun postReview(reviewDto: ReviewDto): Either<DomainError, Success>
+}
---
+++ androidApp/src/main/java/com/eliasasskali/tfg/android/data/repository/ReviewsRepositoryImpl.kt
@@ -0,0 +1,40 @@
+package com.eliasasskali.tfg.android.data.repository
+
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import com.eliasasskali.tfg.android.data.dataSource.ReviewsPagingSource
+import com.eliasasskali.tfg.model.*
+import com.google.firebase.firestore.FieldValue
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.tasks.await
+
+class ReviewsRepositoryImpl(
+    private val config: PagingConfig
+) : ReviewsRepository {
+    override fun getReviews(clubId: String) = Pager(
+        config = config
+    ) {
+        ReviewsPagingSource(
+            queryPosts = FirebaseFirestore
+                .getInstance()
+                .collection("Reviews"),
+            clubId = clubId
+        )
+    }.flow
+
+    override suspend fun postReview(reviewDto: ReviewDto): Either<DomainError, Success> {
+        val reviewsRef = FirebaseFirestore
+            .getInstance()
+            .collection("Reviews")
+
+        return try {
+            reviewsRef
+                .add(reviewDto)
+                .await()
+
+            Either.Right(Success)
+        } catch (e: Exception) {
+            Either.Left(DomainError.ErrorPostingReview)
+        }
+    }
+}
